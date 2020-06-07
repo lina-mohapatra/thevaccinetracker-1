5bb978240dd3364abf75ed18f268d459084a8b84
Rohit Bind <rohit.bind@gmail.com>
2020-06-07 16:46:48 +0530

    Updates

---
+++ component---src-pages-robots-txt-js-7789e21dc3e2d17819fb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{y9P3:function(t,e,n){"use strict";n.r(e);var r=n("q1tI"),o=n.n(r),p=n("vrFN");var a=function(t){var e,n;function r(){return t.apply(this,arguments)||this}return n=t,(e=r).prototype=Object.create(n.prototype),e.prototype.constructor=e,e.__proto__=n,r.prototype.render=function(){return o.a.createElement("div",null,o.a.createElement(p.a,null))},r}(r.Component);e.default=a}}]);
+//# sourceMappingURL=component---src-pages-robots-txt-js-7789e21dc3e2d17819fb.js.map
