65843a412f2993f2cb6f8f4970ac5004c6631301
ujjwal8880 <ujjwal.singhal@rentomojo.com>
2020-06-18 16:46:27 +0530

    Updates

--- component---src-pages-justtestsitemap-js-6f95461dab0718bd5bf7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{wn90:function(t,e,n){"use strict";n.r(e);var r=n("q1tI"),o=n.n(r),p=n("vrFN");var a=function(t){var e,n;function r(){return t.apply(this,arguments)||this}return n=t,(e=r).prototype=Object.create(n.prototype),e.prototype.constructor=e,e.__proto__=n,r.prototype.render=function(){return o.a.createElement("div",null,o.a.createElement(p.a,null))},r}(r.Component);e.default=a}}]);
-//# sourceMappingURL=component---src-pages-justtestsitemap-js-6f95461dab0718bd5bf7.js.map
